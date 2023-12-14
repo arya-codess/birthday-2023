578fe579e3331406205f99523ee099e738bb27fd
arya <143223848+arya-codess@users.noreply.github.com>
2023-12-14 10:48:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.getElementById('yesButton').addEventListener('click', function() {
+  document.getElementById('response').innerText = "YAYY!";
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+  document.getElementById('response').innerText = "aww :(";
+});
